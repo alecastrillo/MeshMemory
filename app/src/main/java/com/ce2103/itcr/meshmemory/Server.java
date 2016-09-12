fb12d4dcff37564a20042cd032e700f41d4f60bd
estape11 <thekillers11>
2016-09-12 10:45:24 -0600

    Test repo

--- app/src/main/java/com/ce2103/itcr/meshmemory/Server.java
+++ app/src/main/java/com/ce2103/itcr/meshmemory/Server.java
@@ -62,7 +62,7 @@ public class Server extends Thread {
                             if(mensaje.contains("client")){
                                 //read_client()
                             }else if (mensaje.contains("node")){
-                                //read_node(); agregar este método
+                                //read_node(); agregar este métodop
                             }
                             System.out.println("Recibido: " + mensaje);
                         }
